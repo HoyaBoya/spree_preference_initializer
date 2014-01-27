016002ebe75d648a0dfc6b25fdd9a4a9159f180b
Hoyaboya <christopher.sun@gmail.com>
2014-01-27 11:11:47 -0500

    first commit

---
+++ lib/spree_preference_initializer/spree/preference_initializer.rb
@@ -0,0 +1,91 @@
+# This class takes standard YML configurations and loads them into the appropriate Spree preference based models.
+module Spree
+  class PreferenceInitializer
+
+    CONFIG_CLASS =    'config_class'
+    GATEWAY_CLASS =   'gateway_class'
+    GATEWAY_ID =      'gateway_id'
+
+    @@warnings = []
+
+    class << self
+
+      def warnings
+        @@warnings
+      end
+
+      def reset!
+        @@warnings = []
+      end
+
+      def load_config_property(context, property, file = "#{Rails.root}/config/config.yml")
+        YAML.load_file(file)[Rails.env][context][property]
+      end
+
+      ##
+      # Load config.yml into a Spree preference model or global Spree configurations.
+      #
+      def load_configs(file = "#{Rails.root}/config/config.yml")
+        config = nil
+
+        # Load configurations in config.yml to the appropriate configuration object.
+        # This allows Devops to set configurations via chef, but keep using the existing Spree framework for preferences.
+        # http://guides.spreecommerce.com/developer/preferences.html
+        YAML.load_file(file)[Rails.env].each_pair do |key, value|
+          # Get the config class that owns the configuration
+          config = value[CONFIG_CLASS].constantize.new
+          # Write the configuration
+          value.each_pair do |app_key, app_value|
+            if app_key == CONFIG_CLASS
+              next
+            end
+
+            unless config.has_preference?(app_key)
+              msg = "#{app_key} is not a preference on #{config}"
+              Rails.logger.warn msg
+              @@warnings << msg
+              next
+            end
+
+            # If the property isn't what we want, then set
+            if config[app_key] != app_value
+              config[app_key] = app_value
+            end
+          end
+        end
+
+        # Insure that everything was injected or explode
+        if config.respond_to?(:valid?) && config.valid? == false
+          raise "#{config_class} IS NOT VALID #{config.errors.messages}"
+        end
+      end
+
+      ##
+      # Load gateway information
+      #
+      def load_gateways(file = "#{Rails.root}/config/gateways.yml")
+        begin
+          # Load Gateway configurations from a specific YML.
+          # This allows us to configure outside of Spree Admin.
+          # The logic is different to how Spree models Gateway preferences vs standard preferences like above (they are not app global).
+          YAML.load_file(file)[Rails.env].each_pair do |key, value|
+            gateway_id = value[GATEWAY_ID]
+            gateway = value[GATEWAY_CLASS].constantize.find_by_id(gateway_id)
+            value.each_pair do |app_key, app_value|
+              # If we have a gateway and property is not set to what we want
+              if gateway && ![GATEWAY_ID, GATEWAY_CLASS].include?(app_key) && gateway.get_preference(app_key) != app_value
+                gateway.set_preference(app_key, app_value)
+              end
+            end
+          end
+        # On any DB that has 0 migrations, we need to catch errors to allow the migration to proceed.
+        rescue ActiveRecord::StatementInvalid => e 
+          Honeybadger.notify(e) if defined?(Honeybadger)
+          Rails.logger.warn(e.message)
+          Rails.logger.warn(e.backtrace.split('\n'))
+          @@warnings << e.message
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/spree/preference_initializer_spec.rb
@@ -0,0 +1,135 @@
+require 'spec_helper'
+require 'rake'
+require 'spree_preference_initializer/spree/preference_initializer'
+
+describe Spree::PreferenceInitializer do
+  before(:each) do
+    Spree::PreferenceInitializer.reset!
+  end
+
+  describe 'load_config_property' do
+    context 'when asking for foo' do
+      it 'should return bar' do
+        Spree::PreferenceInitializer.load_config_property('spree_backend_configuration', 'foo', File.dirname(__FILE__) + "/config.yml").should == 'bar'
+      end
+    end  
+  end
+
+  describe 'load_gateways' do
+    context 'when given the Rails gateways.yml' do
+      subject { Spree::PreferenceInitializer.load_gateways(File.dirname(__FILE__) + "/gateways.yml") }
+
+      it 'should not explode' do
+        subject
+      end
+    end
+
+    context 'when a non DB error is raised' do
+      subject { Spree::PreferenceInitializer.load_gateways(File.dirname(__FILE__) + "/malformed_config.yml") }
+
+      it 'should not catch error' do
+        lambda {
+          subject
+        }.should raise_error
+      end
+    end
+
+##
+# Dicey test to simulate the 0 migration issue when using the preference initialzier.
+#
+# They are commented out to avoid annoying everyone with extra long tests just to validate this scenario.
+#
+=begin
+    context 'when the DB does not exist' do
+      before(:all) do
+        Rails.env = ENV['RAILS_ENV'] = 'test'
+        @rake = Rake::Application.new
+        Rake.application = @rake
+        @rake.init
+        @rake.load_rakefile
+        @rake['db:drop'].invoke
+        @rake['db:create'].invoke
+      end 
+
+      subject { Spree::PreferenceInitializer.load_gateways }
+
+      it 'should not explode' do
+        subject
+      end
+
+      it 'should have warnings' do
+        subject
+        Spree::PreferenceInitializer.warnings.size.should == 1
+      end
+      
+      it 'should notify Honeybadger' do
+        Honeybadger.should_receive(:notify) 
+        subject
+      end
+
+      after(:all) do
+        @rake['db:migrate'].invoke
+      end
+    end
+=end
+
+  end
+
+  describe 'load_config' do
+    context 'when config is malformed' do
+      subject { Spree::PreferenceInitializer.load_configs(File.dirname(__FILE__) + "/malformed_config.yml") }    
+
+      it 'should raise an error' do
+        lambda {
+          subject
+        }.should raise_error
+      end
+    end
+
+    context 'when config class is missing property' do
+      subject { Spree::PreferenceInitializer.load_configs(File.dirname(__FILE__) + "/bad_config.yml") }
+
+      it 'should not raise an error' do
+        lambda {
+          subject
+        }.should_not raise_error
+      end
+    end
+
+    context 'when config class is not valid' do
+      subject { Spree::PreferenceInitializer.load_configs(File.dirname(__FILE__) + "/another_bad_config.yml") } 
+
+      it 'should not raise an error' do
+        lambda {
+          subject
+        }.should raise_error
+      end 
+    end
+
+    context 'when configuration class contains foo' do
+      subject { Spree::PreferenceInitializer.load_configs(File.dirname(__FILE__) + "/config.yml") }
+
+      it 'should set foo' do
+        subject
+        Spree::GoodConfiguration.new.foo.should == 'bar' 
+      end
+    end
+  end
+
+  protected
+
+  class Spree::GoodConfiguration < Spree::Preferences::Configuration
+    preference :foo,      :string
+  end
+
+  class Spree::BadConfiguration < Spree::Preferences::Configuration
+  end
+
+  class Spree::AnotherBadConfiguration < Spree::Preferences::Configuration
+    include ActiveModel::Validations
+
+    preference :something, :string
+
+    validates :something, presence: true
+  end
+end
